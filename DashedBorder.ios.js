0921daea4245d56421404aec5d5d019b8b435dac
chirag04 <jain_chirag04@yahoo.com>
2015-06-11 11:30:54 -0400

    use new style of require for native components

--- DashedBorder.ios.js
+++ DashedBorder.ios.js
@@ -5,17 +5,7 @@
 
 'use strict';
 
-var ReactNativeViewAttributes = require('ReactNativeViewAttributes');
-var createReactNativeComponentClass = require('createReactNativeComponentClass');
-var merge = require('merge');
-var deepDiffer = require('deepDiffer');
-
-var DashedBorder = createReactNativeComponentClass({
-  validAttributes: merge(ReactNativeViewAttributes.UIView, {
-    color:           {diff: deepDiffer},
-    lineDashPattern: {diff: deepDiffer},
-  }),
-  uiViewClassName: 'RNDashedBorder',
-});
+var { requireNativeComponent, } = require('react-native');
+var DashedBorder = requireNativeComponent('RNDashedBorder', null);
 
 module.exports = DashedBorder;
