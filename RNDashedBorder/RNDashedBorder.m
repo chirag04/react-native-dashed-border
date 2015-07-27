4532b3150b14150ae5944488b0c24020615d744f
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-07-27 11:48:52 -0400

    used a simple version. no need for custom function

--- RNDashedBorder/RNDashedBorder.m
+++ RNDashedBorder/RNDashedBorder.m
@@ -11,26 +11,6 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
-// This function copied from
-// http://stackoverflow.com/questions/12134608/cashapelayer-cornerradius-isnt-working-on-uibezierpath
-CGPathRef CGPathCreateRoundRect( const CGRect r, const CGFloat cornerRadius )
-{
-  CGMutablePathRef p = CGPathCreateMutable() ;
-
-  CGPathMoveToPoint( p, NULL, r.origin.x + cornerRadius, r.origin.y ) ;
-
-  CGFloat maxX = CGRectGetMaxX( r ) ;
-  CGFloat maxY = CGRectGetMaxY( r ) ;
-
-  CGPathAddArcToPoint( p, NULL, maxX, r.origin.y, maxX, r.origin.y + cornerRadius, cornerRadius ) ;
-  CGPathAddArcToPoint( p, NULL, maxX, maxY, maxX - cornerRadius, maxY, cornerRadius ) ;
-
-  CGPathAddArcToPoint( p, NULL, r.origin.x, maxY, r.origin.x, maxY - cornerRadius, cornerRadius ) ;
-  CGPathAddArcToPoint( p, NULL, r.origin.x, r.origin.y, r.origin.x + cornerRadius, r.origin.y, cornerRadius ) ;
-
-  return p ;
-}
-
 @implementation RNDashedBorder {
   CAShapeLayer *_border;
 }
@@ -49,8 +29,7 @@ CGPathRef CGPathCreateRoundRect( const CGRect r, const CGFloat cornerRadius )
 - (void)layoutSubviews
 {
   [super layoutSubviews];
-  //if borderRadius is zero, the shape will be rectangle - dashed.
-  _border.path = CGPathCreateRoundRect(self.bounds, self.borderRadius);
+  _border.path = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:self.borderRadius].CGPath;
   _border.frame = self.bounds;
 }
 
